fd2e5fa72aadeb3c83f39bfb4fcaf82c0855b11f
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-11 11:05:16 +0100

    HTMX implementation

--- src/main/java/example/micronaut/TodoItemController.java
+++ src/main/java/example/micronaut/TodoItemController.java
@@ -1,11 +1,17 @@
 package example.micronaut;
 
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
+import io.micronaut.views.ModelAndView;
 import io.micronaut.views.View;
+import io.micronaut.views.htmx.HtmxRequestHeaders;
+import io.micronaut.views.htmx.HtmxResponseHeaders;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -49,16 +55,42 @@ class TodoItemController {
 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Post("/save")
-    HttpResponse<?> addNewTodoItem(@Body TodoItemFormData formData) {
-        repository.save(todoItemMapper.toEntity(formData));
+    HttpResponse<?> addNewTodoItem(@Body TodoItemFormData formData, @Nullable HtmxRequestHeaders htmxRequestHeaders) {
+        TodoItem item = repository.save(todoItemMapper.toEntity(formData));
+        if (htmxRequestHeaders != null) {
+            return HttpResponse.ok(new ModelAndView<>("fragments", Collections.singletonMap(MODEL_ITEM, item)))
+                    .header(HtmxResponseHeaders.HX_TRIGGER, "itemAdded");
+        }
         return HttpResponse.seeOther(ROOT);
+    }
+
+    @Get("/active-items-count")
+    @View("active-items-count")
+    Map<String, Object> htmxActiveItemsAcount(@NonNull HtmxRequestHeaders htmxRequestHeaders) {
+        return Collections.singletonMap("numberOfActiveItems", getNumberOfActiveItems());
     }
 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Post("/{id}/toggle")
     HttpResponse<?> toggleSelection(@PathVariable Long id) {
-        repository.updateCompletedById(true, id);
+        TodoItem todoItem = repository.findById(id)
+                .orElseThrow(() -> new TodoItemNotFoundException(id));
+        repository.updateCompletedById(!todoItem.completed(), id);
         return HttpResponse.seeOther(ROOT);
+    }
+
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Put("/{id}/toggle")
+    HttpResponse<?> htmxToggleTodoItem(@PathVariable Long id, @NonNull HtmxRequestHeaders htmxRequestHeaders) {
+        TodoItem todoItem = repository.findById(id)
+                .orElseThrow(() -> new TodoItemNotFoundException(id));
+        TodoItem updated = new TodoItem(todoItem.id(),
+                todoItem.title(),
+        !todoItem.completed());
+        repository.updateCompletedById(updated.completed(), id);
+        return HttpResponse.ok(new ModelAndView<>("fragments",
+                            Collections.singletonMap(MODEL_ITEM, updated)))
+                    .header(HtmxResponseHeaders.HX_TRIGGER, "itemCompletionToggled");
     }
 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -73,6 +105,14 @@ class TodoItemController {
     HttpResponse<?> deleteTodoItem(@PathVariable Long id) {
         repository.deleteById(id);
         return HttpResponse.seeOther(ROOT);
+    }
+
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Delete("/{id}")
+    HttpResponse<?> deleteTodoItem(@PathVariable Long id,
+                                   @NonNull HtmxRequestHeaders htmxRequestHeaders) {
+        repository.deleteById(id);
+        return HttpResponse.ok().body("").header(HtmxResponseHeaders.HX_TRIGGER, "itemDeleted");
     }
 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -96,7 +136,7 @@ class TodoItemController {
     }
 
     private int getNumberOfActiveItems() {
-        return repository.countByCompleted(true);
+        return repository.countByCompleted(false);
     }
 
     private int getNumberOfCompletedItems() {
